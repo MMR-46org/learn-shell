34e157ae0dd108380d07ba1d4f4220c25b09edd0
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-20 12:15:14 -0500

    loops in shell code

--- 07.loops.sh
+++ 07.loops.sh
@@ -12,9 +12,24 @@ for i in aws devops azure gcp testing;do
 done
 
 
+## break
+
 i=10
 while [ "$i" -gt 0 ];do
   echo hello world
-  i=$(($i - 1))
+  i=$(($i-1))
+  break
+  echo hello universe
+done
+
+
+##continue
+
+i=10
+while [ "$i" -gt 0 ];do
+  echo hello world
+  i=$(($i-1))
+  continue
+  echo hello universe
 done
 
