1e1852ef0938b7d86958dbfc8a4cc4d1837a1940
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-20 11:39:56 -0500

    loops in shell code

--- 07.loops.sh
+++ 07.loops.sh
@@ -10,3 +10,18 @@ done
 for course in aws devops azure gcp testing;do
   echo welcome to course - ${course}
 done
+
+
+
+
+i=10
+
+while [ "$i" -gt 0 ];do
+  echo hello world
+  i=$(($i-1))
+done
+
+
+for i in aws azure devops training gcp;do
+  echo welcome to the course - $i
+done
