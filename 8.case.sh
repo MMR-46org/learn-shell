2b4ebadcdd7f056c0015c33d36a5b1c19c99256b
madhan-46 <madhanmohanreddyeeda@gmail.com>
2024-01-20 12:46:25 -0500

    case command

---
+++ 8.case.sh
@@ -0,0 +1,13 @@
+course=$1
+case $course in
+  aws)
+    echo welcome to aws training
+    ;;
+  devops)
+    echo welcome to devops training
+    ;;
+  *)
+    echo unknown training
+    ;;
+esac
+
